882a4cb264c54cdb769f62adadf665b8957e8d9f
mikemike112-1 <61212233+mikemike112-1@users.noreply.github.com>
2021-03-28 04:30:35 -0400

    Update &help.js

--- codeFolder/commands/&help.js
+++ codeFolder/commands/&help.js
@@ -5,7 +5,7 @@ module.exports = {
             
             var instructions = " ";
 
-            if ((message.author.toString() == '<@666088354147729420>' || message.author.toString() == '<@427616391206666252>')){
+            if ((message.author.toString() == '<@USER_ID>' || message.author.toString() == '<@USER_ID>')){
                 instructions = "Hi! commands are: \n &help \n &orders bought \n &orders sold \n &bought \n &sold \n &delete";
             }   else {
                 instructions = "Hi! commands are \n &help \n &orders bought \n &orders sold";
