870af879bac9740920d3f53818162502067865c2
mikemike112-1 <61212233+mikemike112-1@users.noreply.github.com>
2021-05-02 10:40:15 -0400

    Add files via upload

--- codeFolder/commands/&help.js
+++ codeFolder/commands/&help.js
@@ -5,7 +5,7 @@ module.exports = {
             
             var instructions = " ";
 
-            if ((message.author.toString() == '<@USER_ID>' || message.author.toString() == '<@USER_ID>')){
+            if ((message.author.toString() == '<@666088354147729420>' || message.author.toString() == '<@427616391206666252>')){
                 instructions = "Hi! commands are: \n &help \n &orders bought \n &orders sold \n &bought \n &sold \n &delete";
             }   else {
                 instructions = "Hi! commands are \n &help \n &orders bought \n &orders sold";
